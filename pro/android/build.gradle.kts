68a14ac1f0d9978dd50c0e2af98d7c37aa339288
ambe7888 <ambe.akponi@uvci.edu.ci>
2026-08-14 00:28:28 +0000

    fix: enforce Java 11 compileOptions for all Android subproject plugins

--- pro/android/build.gradle.kts
+++ pro/android/build.gradle.kts
@@ -13,6 +13,14 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    afterEvaluate {
+        project.extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+    }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
